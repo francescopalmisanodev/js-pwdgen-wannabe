73eac45833c368c6e8ed7fd5cfafb7415188a7b7
francescopalmisanodev <francescopalmisano.dev@gmail.com>
2021-10-20 16:43:01 +0200

    v1.0

---
+++ main.js
@@ -0,0 +1,5 @@
+let nome = prompt("inserisci il tuo nome:");
+let cognome = prompt("inserisci il tuo cognome:");
+let colore = prompt("inserisci il tuo colore preferito:");
+let numero = parseInt (prompt("inserisci il tuo numero preferito:"));
+document.getElementById ("password").innerHTML= nome + cognome + colore + numero;
